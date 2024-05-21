07b9efae8164135e206d2506b7daa840a4ac36ea
ToS4 <ayo.mohamad@hotmail.com>
2024-05-21 12:17:05 +0200

    added MVVM

---
+++ AHIFventory/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHIFventory.Model
+{
+    public class Product : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ doc/Project_Requirements.md
@@ -0,0 +1,26 @@
+# AHIFventory
+
+## Description
+* Inventory Management
+* for small Companies
+* functions for managing Products
+
+## Must-Have
+* Add, edit and delete products
+* correct Display of items
+* notifications for low quantity
+* Designed UI
+* DATABASE Integration
+
+## Nice-To-Have
+* Order management
+* MVVM-structure (kinda)
+* Import, Export (PDF, CSV)
+* Email-Notifications
+* Logging
+* Analytics
+
+## No-Have
+* Cloud
+* Multi-Language
+* Multiplatform
