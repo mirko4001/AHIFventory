18e67f56debaf7c22138b857cd9f1035c27fcef4
mirko4001 <hyyazan234@gmail.com>
2024-05-22 09:05:32 +0200

    setup ViewModel

---
+++ AHIFventory/ViewModel/ProductViewModel.cs
@@ -0,0 +1,48 @@
+﻿using AHIFventory.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHIFventory.ViewModel
+{
+    public class ProductViewModel
+    {
+        public ObservableCollection<Product> Products { get; set; }
+
+        public ProductViewModel() { }
+
+        //private void LoadProducts()
+        //{
+        //    using (var connection = new SQLiteConnection(_connectionString))
+        //    {
+        //        connection.Open();
+        //        string query = "SELECT * FROM Fragen";
+
+        //        using (var command = new SQLiteCommand(query, connection))
+        //        using (var reader = command.ExecuteReader())
+        //        {
+        //            while (reader.Read())
+        //            {
+        //                var frage = new Frage
+        //                {
+        //                    ID = reader.GetInt32(reader.GetOrdinal("ID")),
+        //                    FrageText = reader.GetString(reader.GetOrdinal("Frage")),
+        //                    AntwortA = reader.GetString(reader.GetOrdinal("AntwortA")),
+        //                    AntwortB = reader.GetString(reader.GetOrdinal("AntwortB")),
+        //                    AntwortC = reader.GetString(reader.GetOrdinal("AntwortC")),
+        //                    AntwortD = reader.GetString(reader.GetOrdinal("AntwortD")),
+        //                    Richtige = reader.GetString(reader.GetOrdinal("Richtige")),
+        //                    CountSolvedCorrect = reader.GetInt32(reader.GetOrdinal("countSolvedCorrect")),
+        //                    CountSolvedIncorrect = reader.GetInt32(reader.GetOrdinal("countSolvedIncorrect"))
+        //                };
+
+        //                this.Add(frage);
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}
