85728afa25079a6bef30d696e17839398bfe9833
ToS4 <ayo.mohamad@hotmail.com>
2024-06-11 11:56:35 +0200

    namespace changes for all files

--- AHIFventory/View/ProductPage.xaml.cs
+++ AHIFventory/View/ProductPage.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace AHIFventory
 {
     /// <summary>
-    /// Interaction logic for HomePage.xaml
+    /// Interaction logic for ProductPage.xaml
     /// </summary>
-    public partial class HomePage : Page
+    public partial class ProductPage : Page
     {
-        public HomePage()
+        public ProductPage()
         {
             InitializeComponent();
         }
